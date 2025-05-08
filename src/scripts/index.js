5fbe939a66fc2d955ebeb65537def3ec109caeaf
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-08 15:46:17 -0300

    update content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,23 @@
-window.botpressWebChat.init({
+window.botpress.on("webchat:ready", () => {
+    window.botpress.open();
+});
 
-    configUrl: "https://files.bpcontent.cloud/2025/05/07/16/20250507161047-TIXN3XZ6.json",
-    botConversationLanguage: "pt",
-    showPoweredBy: false,
-    botConversationDescription: "Assistente da Chácara",
-    botId: "chacara-assistente",
-    position: "right",
+window.botpress.init({
+    "botId": "d936cf5b-e36d-46d9-b7bd-26e9adb0fd9b",
+    "configuration": {
+        "website": {},
+        "email": {},
+        "phone": {},
+        "termsOfService": {},
+        "privacyPolicy": {},
+        "color": "#5eb1ef",
+        "variant": "soft",
+        "themeMode": "light",
+        "fontFamily": "inter",
+        "radius": 1,
+        "botConversationLanguage": "pt",
+        "showPoweredBy": false,
+    },
+    "clientId": "abaf3b01-c4e1-4d95-850e-1c12844a748d",
+    "selector": "#webchat"
 });
