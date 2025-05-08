ca893b097224e945a64d9d0fd35a5f98ac9b3766
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2025-05-08 14:34:36 -0300

    update content

--- src/css/scripts/index.js
+++ src/css/scripts/index.js
@@ -1,8 +1,8 @@
 window.botpressWebChat.init({
-    
+
     "configUrl": "https://files.bpcontent.cloud/2025/05/07/16/20250507161047-TIXN3XZ6.json",
     "showPoweredBy": false,
     "botConversationDescription": "Assistente da Chácara",
     "botId": "chacara-assistente",
     "botConversationLanguage": "pt"
-  });
+});
