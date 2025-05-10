c87fed0ccacdda91c756301609ae9eccebd30e74
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-10 19:44:19 -0300

    update code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,12 +12,13 @@ window.botpress.init({
         "fontFamily": "inter",
         "radius": 1,
         "botConversationLanguage": "pt",
-        "showPoweredBy": false,
+        // "showPoweredBy": false,
+        "botName": "Assistente Chácara Amaral",
     },
     "clientId": "abaf3b01-c4e1-4d95-850e-1c12844a748d",
     "selector": "#webchat"
 });
 
-window.botpress.on("webchat:ready", () => {
-    window.botpress.open();
-});
+// window.botpress.on("webchat:ready", () => {
+//     window.botpress.open();
+// });
